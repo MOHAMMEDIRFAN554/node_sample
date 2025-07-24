3857d49984d45c0acd1526b69820638d391c3a48
MOHAMMEDIRFAN554 <98808506+MOHAMMEDIRFAN554@users.noreply.github.com>
2025-07-24 09:38:22 +0530

    task1

---
+++ isprime.js
@@ -0,0 +1,25 @@
+function isPrime(num) {
+    if (num <= 1) {
+        console.log(`${num} is not a Prime Number`);
+        return;
+    }
+    else if (num == 2){
+        console.log(`${num} is a Prime Number`);
+        return;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            console.log(`${num} is not a Prime Number`);
+            return;
+        }
+        else{
+            console.log(`${num} is a Prime Number`);
+            return;
+        }
+    }
+}
+isPrime(2);    
+isPrime(9);    
+isPrime(17);   
+isPrime(100);  
+isPrime(1);   
---
+++ maxprime.js
@@ -0,0 +1,33 @@
+function isPrime(num) {
+    if (num <= 1) {
+        // console.log(`${num} is not a Prime Number`);
+
+        return false;
+    } else if (num === 2) {
+        // console.log(`${num} is a Prime Number`);
+
+        return true;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            // console.log(`${num} is not a Prime Number`);
+
+            return false;
+        }
+    }
+    // console.log(`${num} is a Prime Number`);
+
+
+    return true;
+}
+
+function maxPrime(n) {
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+
+maxPrime(50);
