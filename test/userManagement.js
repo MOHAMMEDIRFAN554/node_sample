08ce05e586d3d0daed6cdf04c1bf0cea3f05fa4b
MOHAMMEDIRFAN554 <98808506+MOHAMMEDIRFAN554@users.noreply.github.com>
2025-08-13 09:48:28 +0530

    e

--- test/userManagement.js
+++ test/userManagement.js
@@ -1,41 +1,58 @@
 const http = require('http')
 const PORT = 4200
 
-let users = ["john","madhav","ajith"]
+let users = ["john", "madhav", "ajith"]
 
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
     let url = req.url
 
-    if(url.includes("getAllUser")){
-        res.end(`list of users = ${users}`)
-        
+    if (url.includes("getAllUser")) {
+        res.end(`the users are = ${users}`)
     }
-    else if(url.includes("insertUser")){
-        let name = url.split("=")[1]
-        
-        if(users.includes(name)){
-            res.end(`the ${name} is already exists`)
+    else if (url.includes("insertUser")) {
+        let str = url.split("=")[1]
+
+        if (!str?.trim()) {
+            res.end("User name cannot be empty");
         }
-        else{
-            users.push(name)
-            res.end(`the ${name} is added to database`)
+
+        else if (users.includes(str)) {
+            res.end(`${str} is already in database`)
+        } else {
+
+            users.push(str)
+            res.end(`${str} added successfully ,updated array = ${users}`)
+
         }
     }
+    else if (url.includes("deleteUser")) {
+        let str = url.split("=")[1]
+
+        index = users.indexOf(str)
 
 
+        if (index == -1) {
+            res.end(`${str} is not found in database`)
+        }
+        else {
+            users.splice(index, 1)
+            res.end(`${str} is successfully deleted `)
+        }
 
-    
-
+    }
+    else {
+        res.end(`invalid input please check getAllUser, insertUser, deleteUser`)
+    }
 
 
 })
 
-server.listen(PORT, function(err){
-    if(err){
-        console.log("EROOR",err)
+server.listen(PORT, function (err) {
+    if (err) {
+        console.log("EROOR", err)
     }
-    else{
+    else {
         console.log("Server started successfully")
     }
 })
