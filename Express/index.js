03d63b56e8100a74884956ccce568658fc2ee39c
MOHAMMEDIRFAN554 <98808506+MOHAMMEDIRFAN554@users.noreply.github.com>
2025-08-22 22:31:30 +0530

    edu

--- Express/index.js
+++ Express/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 let PORT = 4500
-
 let funct = require('./functions')
-
 const app = express();
 app.use(express.json())
-
 app.get('/', (req, res) => {
     res.send('check \n /square?n=5 \n /palindrome?str=hello  \n /sum?num1=2&num2=5 \n /greet?fname=FNAME&lname=LNAME');
 });
-
-
 app.get('/square', (req, res) => {
     let num = req.query.n
     if (isNaN(num)) {
@@ -22,7 +17,6 @@ app.get('/square', (req, res) => {
         let sqr = num * num
         res.send(`square of number ${num} is ${sqr}`)
     }
-
 })
 app.get('/palindrome', (req, res) => {
 
@@ -37,18 +31,12 @@ app.get('/sum', (req, res) => {
     let num2 = parseInt(req.query.num2)
     let sum = num1 + num2
     res.send(`sum of ${num1} and ${num2} = ${sum}`)
-
 })
-
-app.get('/greet',(req,res)=>{
+app.get('/greet', (req, res) => {
     let fname = req.query.fnamee
     let lname = req.query.lname
-
-    res.send("Hello "+fname +" "+lname)
-    
-
+    res.send("Hello " + fname + " " + lname)
 })
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
